1178f12ff44f63139f142060e7a9022fe9a3e7ef
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:50:09 +0530

    Add src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Community Garden Website!</h1>
+      <p>Explore and join us in our sustainability journey.</p>
+    </div>
+  );
+};
+
+export default Home;
