2c063ccd5d1a8ea088607428cd52059844a0429c
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-22 21:50:08 +0530

    Add src/components/Event.js

---
+++ src/components/Event.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Event = () => {
+  return (
+    <div>
+      <h2>Upcoming Event</h2>
+      <p>Details about the upcoming event will be displayed here.</p>
+    </div>
+  );
+};
+
+export default Event;
